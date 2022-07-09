3448266c905e38040766e42a0a45673430a4410c
Konstantin Surikov <sk@Tas-MacBook-Air.local>
2022-07-09 13:00:39 +0300

    Added first React components

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,10 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import Main from '../../pages/main/main';
+
+type AppProps = {
+  cardsCount: number;
+}
+function App({cardsCount}: AppProps): JSX.Element {
+  return <Main cardsCount={cardsCount}/>;
 }
 
 export default App;
--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 
+const settings = {
+  cardsCount: 6,
+};
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 
 root.render(
   <React.StrictMode>
-    <App />
+    <App cardsCount={settings.cardsCount}/>
   </React.StrictMode>,
 );
