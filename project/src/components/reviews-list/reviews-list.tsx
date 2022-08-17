e0768da2f81cb79a20595b8e725c0b05cd307116
Konstantin Surikov <surikov@gmail.com>
2022-08-17 16:42:55 +0300

    Fixed the reviews list

--- project/src/components/reviews-list/reviews-list.tsx
+++ project/src/components/reviews-list/reviews-list.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import {Review} from '../../types/review';
 import ReviewItem from '../review-item/review-item';
 
+const MAX_REVIEWS = 10;
+
 type ReviewListProps = {
   reviews: Review[]
 }
@@ -9,7 +11,11 @@ type ReviewListProps = {
 const ReviewsList = ({reviews}: ReviewListProps) => (
   <ul className="reviews__list" data-testid="reviews-list">
     {
-      reviews.map((review) => <ReviewItem key={review.id} review={review}/>)
+      [...reviews]
+        .sort((reviewA, reviewB) =>
+          new Date(reviewB.date).getTime() - new Date(reviewA.date).getTime())
+        .slice(0, MAX_REVIEWS)
+        .map((review) => <ReviewItem key={review.id} review={review}/>)
     }
   </ul>
 );
